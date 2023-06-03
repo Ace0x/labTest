f651dc7aa1f8ed4c2abba5d9c7f2b1e4e47fb4c2
Ace0x <59582360+Ace0x@users.noreply.github.com>
2023-06-03 13:15:19 -0600

    build

---
+++ tests/test.py
@@ -0,0 +1,10 @@
+#test Hello | python app.py
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.app import Hello
+
+def test_Hello():
+    assert Hello() == "Hello World!"
+    
